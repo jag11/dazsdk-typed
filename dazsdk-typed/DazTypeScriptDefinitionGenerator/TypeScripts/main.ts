1445f85f18f50bad4432b199b203536c1ffa31bd
Fco. Javier Acevedo Galván <jag11@hotmail.com>
2015-11-08 13:58:51 -0600

    Added missing main.ts

---
+++ dazsdk-typed/DazTypeScriptDefinitionGenerator/TypeScripts/main.ts
@@ -0,0 +1,23 @@
+﻿class DazReflectorApplication extends ApplicationBase {
+    pixmapLabel: DzLabel = null;
+    constructor(appName?: string) {
+        super(appName);
+    }
+
+    initialize() {
+        super.initialize();
+
+        // create a new tab, content will be a DzLabel
+        this.pixmapLabel = new DzLabel(this.tabContainer);
+        this.tabContainer.addTab(this.pixmapLabel, "Pixmap");
+    }
+}
+
+// create instance and run it
+var app = new DazReflectorApplication("Any app name");
+
+logger.info("An info message");
+logger.error("An error message");
+logger.warn("An %1 error message", "darn");
+logger.info(reflector.setObject(app).toString());
+app.run();
